d18b4640dd3ffe8e548d67867ee0edfcf171f962
evaklimentova <klimentova.eva@seznam.cz>
2022-09-09 10:14:40 +0200

    Update to ResNet miRBind

--- mirbind.py
+++ mirbind.py
@@ -5,6 +5,64 @@ import numpy as np
 import pandas as pd
 from tensorflow import keras as k
 import tensorflow_addons as tfa
+from tensorflow.keras import layers
+from tensorflow.keras.utils import register_keras_serializable
+
+
+@register_keras_serializable()
+class ResBlock(layers.Layer):
+    """
+    Defines a Residual block. For more information refer to the original paper at https://arxiv.org/abs/1512.03385 .
+    """
+
+    def __init__(self, downsample=False, filters=16, kernel_size=3):
+
+        super(ResBlock, self).__init__()
+
+        # store parameters
+        self.downsample = downsample
+        self.filters = filters
+        self.kernel_size = kernel_size
+
+        # initialize inner layers
+        self.conv1 = layers.Conv2D(kernel_size=self.kernel_size,
+                                   strides=(1 if not self.downsample else 2),
+                                   filters=self.filters,
+                                   padding="same")
+        self.activation1 = layers.ReLU()
+        self.batch_norm1 = layers.BatchNormalization()
+        self.conv2 = layers.Conv2D(kernel_size=self.kernel_size,
+                                   strides=1,
+                                   filters=self.filters,
+                                   padding="same")
+        if self.downsample:
+          self.conv3 = layers.Conv2D(kernel_size=1,
+                                      strides=2,
+                                      filters=self.filters,
+                                      padding="same")
+
+        self.activation2 = layers.ReLU()
+        self.batch_norm2 = layers.BatchNormalization()
+
+    def call(self, inputs):
+
+        x = self.conv1(inputs)
+        x = self.activation1(x)
+        x = self.batch_norm1(x)
+        x = self.conv2(x)
+
+        if self.downsample:
+            inputs = self.conv3(inputs)
+
+        x = layers.Add()([inputs, x])
+
+        x = self.activation2(x)
+        x = self.batch_norm2(x)
+
+        return x
+
+    def get_config(self):
+        return {'filters': self.filters, 'downsample': self.downsample, 'kernel_size': self.kernel_size}
 
 
 def parse_input():
@@ -16,7 +74,7 @@ def parse_input():
                                                  'binding')
     parser.add_argument('--input', default="example.tsv", metavar='<input_tsv_filename>')
     parser.add_argument('--output', default="example_scores", metavar='<output_filename_prefix>')
-    parser.add_argument('--model', default="Models/CNN_model_1_10_optimized.h5", metavar='<model_name>')
+    parser.add_argument('--model', default="Models/miRBind.h5", metavar='<model_name>')
     args = parser.parse_args()
     return vars(args)
 
@@ -53,7 +111,7 @@ def write_score(output_file, df, scores):
     :param df: dataframe with miRNA:target pairs
     :param scores: numpy array, predicted scores
     """
-    scores = scores.flatten()
+    scores = scores.flatten()[::2]
     df["score"] = pd.Series(scores, index=df.index)
     df.to_csv(output_file + '.tsv', sep='\t', index=False)
 
